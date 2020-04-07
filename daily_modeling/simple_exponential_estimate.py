d41aa434e2ea08296da4646d5a8dd0e5417c1aff
Peter Bryan <peterbbryan@gmail.com>
2020-04-06 22:03:18 -0400

    command line argument calling with REPL using the fire library

--- daily_modeling/simple_exponential_estimate.py
+++ daily_modeling/simple_exponential_estimate.py
@@ -1,7 +1,15 @@
+import fire
+
 from data_access import get_covid_df
 
 
-def weighted_exponential():
+def predicted_number_dead(n_days: int, smoothed_exp_halflife: int = 7) -> int:
+    """
+
+    :param n_days:
+    :param smoothed_exp_halflife:
+    :return:
+    """
 
     # get data from default covid endpoint
     df = get_covid_df()
@@ -15,8 +23,20 @@ def weighted_exponential():
     new_deaths = new_deaths.iloc[1:]
 
     new_deaths["ratio"] = new_deaths["new_daily_deaths"] / new_deaths["new_daily_deaths_shifted"]
-    new_deaths["smoothed_ratio"] = new_deaths["ratio"].ewm(halflife=1).mean()
+    new_deaths["smoothed_ratio"] = new_deaths["ratio"].ewm(halflife=7).mean()
+
+    last_day_death_count = df["new_daily_deaths"].iloc[-1]
+    smoothed_exponent = new_deaths["smoothed_ratio"].iloc[-1]
+
+    total_dead = df["total_deaths"].iloc[-1]
+    forecasted_deaths = [int(round(last_day_death_count * (smoothed_exponent ** day)))
+                         for day in range(1, n_days+1)]
+
+    print(f"Forecasted deaths per day: {forecasted_deaths}")
+
+    return total_dead + sum(forecasted_deaths)
 
 
 if __name__ == "__main__":
-    weighted_exponential()
+
+    fire.Fire(predicted_number_dead)
