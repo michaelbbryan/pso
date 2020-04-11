9ed95c3f22e9d2deb5697c01180d95f417c9374a
Michael <michael.b.bryan@gmail.om>
2020-04-11 15:20:51 -0400

    Splitting the SIR from travel and geog methods

--- infectedgrid.py
+++ infectedgrid.py
@@ -6,7 +6,8 @@
     This models the population on a two dimensional grid, like geog space.
     Exposure is simulated using multivariate normal to people proximate to those infected
 """
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import pandas as pd
 import random
@@ -14,56 +15,84 @@ from default_values import POPULATION, DURATION, PERIOD, INITIAL_INFECTIONS, REC
 
 random.seed(1234)
 
-POPULATION = 1000000
-assert np.sqrt(POPULATION).is_integer()
+assert np.sqrt(POPULATION).is_integer()  # the population should be a square
+
+# If an infected person has two layers of proximate contacts
+# a multivariate normal grid around that person looks like
+PROXIMATE_PROB = np.array([[0.005, 0.005, 0.005, 0.005, 0.005], \
+                           [0.005, 0.080, 0.150, 0.080, 0.005], \
+                           [0.005, 0.150,     0, 0.150, 0.005], \
+                           [0.005, 0.080, 0.150, 0.080, 0.005], \
+                           [0.005, 0.005, 0.005, 0.005, 0.005]])
+
+# Dampen prox probability to 14 day expected value of 3 infections
+for x in range(5):
+    for y in range(5):
+        PROXIMATE_PROB[x,y] = (3/14) * PROXIMATE_PROB[x,y]
 
 # Initialize the city as a list of people
 #      each person starts Infected=False, Susceptibe=True
 infected = np.full((POPULATION), False, dtype=bool)
-infected_duration = np.full((POPULATION), 0, dtype=int)
+infected_duration = np.zeros((POPULATION),dtype=int)
 susceptible = np.full((POPULATION), True, dtype=bool)
 
 # Initialize the simulation results
-results = pd.DataFrame(columns = ['Day' , 'Infected', 'Died' , 'Recoverd'])
+results = pd.DataFrame(columns = ['Day' , 'Infected', 'Died' , 'Recovered', 'Susceptible'])
 
 # Randomly distribute the initial infections
-for p in np.random.choice(range(POPULATION),initial,replace=False):
+initial = np.random.choice(range(POPULATION),INITIAL_INFECTIONS,replace=False)
+for p in initial:
     infected[p] = True
-
+    susceptible[p] = False
 
 for day in range(PERIOD): # iterate over time
-    # first recover or die off those infected for the DURATION
+    print("day",day,"infected",sum(infected))
+    # first recover or die off those who have been infected for the DURATION
+    died = 0
+    recovered = 0
     for i in range(POPULATION):
-        if infected_duration[i] = 14:
-            infected_duration = 0
+        if infected_duration[i] == DURATION:
+            infected_duration[i] = 0
+            infected[i] = False
             susceptible[i] = False
-
-    rows.append({"Day": day, "Died": died, "Recovered": recovered, "Infected": infected[0]})
-
-    RECOVERY_RATE
-
-    for each proximate
-        for offsetx in range(-2,,3,1):
-            for offsety in range(-2,,3,1):
-                if susceptible[x+offsetx,y+offsety]
-        infect only those susceptible
-        applying the distributed probabilities
-
-
-        # the position of these people is given by
-        row = np.floor(infected / np.sqrt(100))
-        col = (infected % np.sqrt(100))
-
-
-        # multivariate normal of contact around an infected person
-        # 0.005	    0.005	0.005	    0.005	0.005
-        # 0.005	    0.08	0.15	    0.08	0.005
-        # 0.005	    0.15	infected	0.15	0.005
-        # 0.005	    0.08	0.15	    0.08	0.005
-        # 0.005	    0.005	0.005   	0.005	0.005
-
-        results = results.append({"Day": day,
-                                  "Died": died,
-                                  "Recovered": recovered,
-                                  "Infected": sum(infected)},
+            if np.random.choice([True, False], 1, p=[RECOVERY_RATE, 1-RECOVERY_RATE]):
+                recovered += 1
+            else:
+                died += 1
+    # next spread the disease from each infected person
+    for i in range(POPULATION):
+        if infected[i]:
+            # project the disease onto the proximate square
+            for y in range(PROXIMATE_PROB.shape[0]):
+                for x in range(PROXIMATE_PROB.shape[1]):
+                    square = int((i + (y - 2) * np.sqrt(POPULATION)) + (x - 2))
+                    if square >= 0 and square < POPULATION:
+                        if susceptible[square]:
+                            if np.random.choice([True, False], 1, p=[PROXIMATE_PROB[x,y], 1 - PROXIMATE_PROB[x,y]]):
+                                susceptible[square] = False
+                                infected[square] = True
+    # increase the time of those infected
+    for i in range(POPULATION):
+        if infected[i]:
+            infected_duration[i] += 1
+    # and capture the day's results
+    results = results.append({"Day": day,
+                              "Died": died,
+                              "Recovered": recovered,
+                              "Infected": sum(infected),
+                              "Susceptible": sum(susceptible)},
                                  ignore_index = True)
+
+def infection_trend(r):
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.plot(r['Day'], r['Infected'])
+    plt.text(160,2,(POPULATION-sum(susceptible)))
+    plt.show()
+
+def heat(s):
+    heatmap = s.astype(int).reshape(int(np.sqrt(POPULATION)),int(np.sqrt(POPULATION)))
+    plt.imshow(heatmap, cmap='hot', interpolation='nearest')
+    plt.show()
+
+heat(susceptible)
